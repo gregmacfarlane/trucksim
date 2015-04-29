09e568a77919ac1e1e982b4a7ac56503c859c4c4
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-04-29 15:43:30 -0400

    Wrote initial controller script.

---
+++ src/trucksim/Controller.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package trucksim;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+
+/**
+ * @author Greg
+ *
+ */
+public class Controller {
+
+	/**
+	 * @param args 
+	 * First argument should be the name of the xml configuration file.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String configfile = args[0];
+		Config config = ConfigUtils.loadConfig(configfile);
+		Controler controler = new Controler(config);
+		
+		controler.setCreateGraphs(false);
+		controler.run();
+
+	}
+
+}
