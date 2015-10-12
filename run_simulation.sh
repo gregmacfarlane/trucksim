5d615a248141ae2935e80c08a6a6d6b89a589340
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-10-12 08:43:06 -0400

    Add memory control to Java command.

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 # This script runs the MATSim scenario and post-processing scripts.
+scenario="5day_5pct_2011_config"
 
 # 1 -------------------------------------
 # This command runs the MATSim scenario based on the configuration file
 # specified.
 echo "Running simulation"
 java -classpath "bin:libs/*"\
- trucksim.Controller ./nc_config.xml
+ -Xmx60G\
+ trucksim.Controller ./${scenario}.xml
 
 
 # 2 ------------------------------------
 # Create run report
 echo "Creating report"
-Rscript Rmd/parameters.R  nc_test nc_outputs
-Rscript -e "rmarkdown::render('Rmd/report.Rmd', output_file = '../nc_test.html')"
+Rscript Rmd/parameters.R  ${scenario} ${scenario}
+Rscript -e "rmarkdown::render('Rmd/report.Rmd', output_file = '../${scenario}.html')"
 
 
 # 3 ------------------------------------
-echo "Getting link daily volumes."
-java -classpath "bin:libs/*"\
- trucksim.VolumeCalculator ./nc_config.xml \
- ./outputs/nc_outputs/ITERS/it.50/nc_test.50.events.xml.gz \
- ./outputs/nc_outputs/daily_link_volumes.csv
+#echo "Getting link daily volumes."
+#java -classpath "bin:libs/*"\
+# trucksim.VolumeCalculator ./${scenario}_config.xml \
+# ./outputs/${scenario}/ITERS/it.10/${scenario}.10.events.xml.gz \
+# ./outputs/${scenario}/daily_link_volumes.csv
